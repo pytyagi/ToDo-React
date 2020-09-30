9ea561b5e350a54ed279f5ead9a91c0921f3a37d
Piyush Tyagi <33159028+pytyagi@users.noreply.github.com>
2020-09-30 16:43:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import uuid from "react-uuid";
+import "./styles.css";
+
+function Header({ addTask, handleChange }) {
+  return (
+    <>
+      <h1 className="head">Task List</h1>
+      <div className="form-input">
+        <textarea
+          type="text"
+          placeholder="Add a task.."
+          onFocus={clearText}
+          onChange={handleChange}
+        />
+        <img
+          class="add"
+          src={require("../src/addIcon.png")}
+          alt=""
+          onClick={addTask}
+        />
+      </div>
+    </>
+  );
+}
+
+const RenderList = ({ task, deleteTask }) => {
+  return (
+    <>
+      {task.length > 0 && (
+        <div className="list-container">
+          {task.map(
+            ({ id, text }) =>
+              text && (
+                <div className="list-item" key={id}>
+                  <div className="text-item">{text}</div>
+                  <img
+                    class="delete"
+                    src={require("../src/deleteIcon.png")}
+                    alt=""
+                    onClick={deleteTask}
+                    id={id}
+                  />
+                </div>
+              )
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+const clearText = (e) => {
+  e.target.value = "";
+};
+
+export default function App() {
+  const [val, setVal] = useState("");
+  const [task, setTask] = useState([]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setVal(e.target.value);
+  };
+  const addTask = () => {
+    setTask([...task, { id: uuid(), text: val }]);
+  };
+  const deleteTask = (e) => {
+    const targetId = e.target.getAttribute("id");
+    const updatedTask = task.filter((item) => item.id !== targetId);
+    setTask(updatedTask);
+  };
+
+  return (
+    <div className="container">
+      <Header addTask={addTask} handleChange={handleChange} />
+      <RenderList task={task} deleteTask={deleteTask} />
+    </div>
+  );
+}
